3c5925cbcb87ccd8bc4819fce80b181f8477d8f1
Lucky <143674097+TEAMGODz@users.noreply.github.com>
2024-01-31 11:55:00 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -8,7 +8,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>┏━━━━━•◦●◉✿ ❟❛❟ ✿◉●◦•━━━━━━┓\n× ᴅᴇᴠᴇʟᴏᴘᴇʀ : <a href='https://t.me/ElitesCrewBot'>Eʟɪᴛᴇs ᴀᴅᴍɪɴs</a>\n× ʜᴇɴᴛᴀɪ ᴄʜᴀɴɴᴇʟ : <a href='https://t.me/+befOBiGrQ7Y2ZGE9'>ʜᴇᴀɴɪᴍᴇ ʜᴜʙ</a>\n× ᴀɴɪᴍᴇ ᴄʜᴀɴɴᴇʟ : <a href='https://t.me/anime_elites'>ᴀɴɪᴍᴇ ᴇʟɪᴛᴇs</a>\n× ʙᴏᴛs ᴜᴘᴅᴀᴛᴇs : <a href='https://t.me/elites_bots'>ᴇʟɪᴛᴇs ʙᴏᴛᴢ</a>\n× sᴇʀɪᴇs ᴄʜᴀɴɴᴇʟ : <a href='https://t.me/+78IZqN1ZqP0zMThl'>sᴇʀɪᴇs ᴇʟɪᴛᴇs</a>\n┗━━━━━•◦●◉✿ ❟❛❟ ✿◉●◦•━━━━━━┛</b>",
+            text = f"<b>┏━━━━━•◦●◉✿ ❟❛❟ ✿◉●◦•━━━━━━┓\n× ᴅᴇᴠᴇʟᴏᴘᴇʀ : <a href='https://t.me/ElitesCrewBot'>Eʟɪᴛᴇs ᴀᴅᴍɪɴs</a>\n× ʜᴇɴᴛᴀɪ ᴄʜᴀɴɴᴇʟ : <a href='https://t.me/+XzLKbp2hxthhNTY1'>Hᴇɴᴛᴀɪ Eʟɪᴛᴇs</a>\n× ᴀɴɪᴍᴇ ᴄʜᴀɴɴᴇʟ : <a href='https://t.me/anime_elites'>ᴀɴɪᴍᴇ ᴇʟɪᴛᴇs</a>\n× ʙᴏᴛs ᴜᴘᴅᴀᴛᴇs : <a href='https://t.me/elites_bots'>ᴇʟɪᴛᴇs ʙᴏᴛᴢ</a>\n× sᴇʀɪᴇs ᴄʜᴀɴɴᴇʟ : <a href='https://t.me/+78IZqN1ZqP0zMThl'>sᴇʀɪᴇs ᴇʟɪᴛᴇs</a>\n┗━━━━━•◦●◉✿ ❟❛❟ ✿◉●◦•━━━━━━┛</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
